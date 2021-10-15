45055dc80866757e0f976aa1ccc314a2d1cdec17
ali-alakbar <ali2akbar5@gmail.com>
2021-10-16 00:11:11 +0300

    Uploaded

---
+++ JS/Script.js
@@ -0,0 +1,111 @@
+
+// ========================= Start Appearing List Icon When Window Size Decreased =========================
+// ===== Calling The List Icon =====
+const list = document.querySelector(".list");
+
+// ===== Calling the Bar of Links =====
+const navi = document.querySelector(".navi");
+
+list.addEventListener(
+    "click",
+
+(eo) => {
+    navi.classList.toggle("show");
+}
+
+);
+// ========================= Start Appearing List Icon When Window Size Decreased =========================
+
+
+// ========================= Start Arthimitic Operations =========================
+
+// ===== Calling All The Inputs Such As Numbers and Symbols =====
+const inputs = document.querySelectorAll("[type=button]");
+// ===== Calling The Container of The Inputs =====
+const calcContianer = document.querySelector(".calc-contianer");
+// ===== Calling Where the Result Will Be Shown =====
+const text = document.getElementById("display");
+
+calcContianer.addEventListener("click",
+
+(eo) => {
+
+
+    switch (eo.target.className) {
+      case "forCss element less":
+        text.value += "<";
+        break;
+      case "forCss element zeros":
+        text.value += "00";
+
+        break;
+
+      case "num9 forCss element":
+        text.value += "9";
+        break;
+
+      case "num8 forCss element":
+        text.value += "8";
+
+        break;
+      case "num7 forCss element":
+        text.value += "7";
+        break;
+
+      case "num6 forCss element":
+        text.value += "6";
+
+        break;
+      case "num5 forCss element":
+        text.value += "5";
+
+        break;
+      case "num4 forCss element":
+        text.value += "4";
+        break;
+
+      case "num3 forCss element":
+        text.value += "3";
+        break;
+
+      case "num2 forCss element":
+        text.value += "2";
+        break;
+      case "num1 forCss element":
+        text.value += "1";
+
+        break;
+
+      case "num0 element":
+        text.value += "0";
+        break;
+      case "comma element":
+        text.value += ".";
+
+        break;
+      case "forCss element division":
+        text.value += "/";
+
+        break;
+
+      case "sub forCss element":
+        text.value += "-";
+
+        break;
+
+      case "multi forCss element":
+        text.value += "*";
+        break;
+      case "add forCss element":
+        text.value += "+";
+
+        break;
+
+      default:
+    }
+
+}
+
+    
+);
+// ========================= End Arthimitic Operations =========================
